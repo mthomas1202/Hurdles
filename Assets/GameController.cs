4b100f38acbe817258b808f08b92deee03954ffa
Matthew Thomas <matt.thomas1202@yahoo.com>
2018-09-07 18:18:55 -0400

    game logic and base game

---
+++ Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameController : MonoBehaviour {
+
+    public TextMesh infoText;
+    public Player player;
+
+    private float gameTimer = 0f;
+    private float restartTimer = 3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.reachedFinishLine == false)
+        {
+            gameTimer += Time.deltaTime;
+            infoText.text = "\n\nAvoid the Obstacles!\nPress space to jump!\nTime: " + Mathf.Floor(gameTimer);
+        }
+        else
+        {
+            infoText.text = "You Win!\nYou're Time: " + Mathf.Floor(gameTimer);
+
+            restartTimer -= Time.deltaTime;
+            if(restartTimer <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float maximumSpeed = 5.5f;
+    public float acceleration = 1f;
+    public float jumpingForce = 450f;
+    public float jumpingCooldown = 0.6f;
+    public bool reachedFinishLine = false;
+    private float jumpingTimer = 0f;
+    private float speed = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        jumpingTimer -= Time.deltaTime;
+
+        //Move the player forward
+        speed += acceleration * Time.deltaTime;
+        if (speed > maximumSpeed)
+        {
+            speed = maximumSpeed;
+        }
+        transform.position += speed * Vector3.forward * Time.deltaTime;
+
+        //Make the player jumper
+        if (Input.GetKeyDown("space"))
+        {
+            if (jumpingTimer <= 0f)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * jumpingForce);
+                jumpingTimer = jumpingCooldown;
+            }
+
+        }
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Obstacle")
+        {
+            speed *= 0.3f;
+        }
+
+        if(collider.tag == "FinishLine")
+        {
+            reachedFinishLine = true;
+        }
+    }
+}
